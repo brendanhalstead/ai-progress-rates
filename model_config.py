a6946a4049fb3be2d53a502f352b7d5b14211fc9
Brendan Halstead <bhalstea@mit.edu>
2025-10-02 00:05:17 -0700

    changed to 99.9th percentile, added some units to the SOS indicators

--- model_config.py
+++ model_config.py
@@ -36,7 +36,7 @@ AGGREGATE_RESEARCH_TASTE_BASELINE = 1.0
 AGGREGATE_RESEARCH_TASTE_FALLBACK = 1.0
 
 # Research Taste Distribution Parameters (Log-Normal)
-TOP_PERCENTILE = 0.01                    # fraction classed as "top" researchers
+TOP_PERCENTILE = 0.001                    # fraction classed as "top" researchers
 MEDIAN_TO_TOP_TASTE_MULTIPLIER = 3.25           # threshold taste ÷ median taste
 
 # Research Taste Schedule Configuration (UI-level options)
